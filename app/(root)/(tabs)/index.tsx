a3d32d82047b405506b25f6347ab5c0b24e03b02
Prish20 <contact@christadriansanya.me>
2025-01-10 17:42:52 +0300

    feat: implement tab navigation layout with icons for Home, Explore, and Profile screens

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from "expo-router";
+
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -11,10 +11,6 @@ export default function Index() {
       }}
       >
         <Text className=" font-rubik text-3xl my-10">Welcome to Restate</Text>
-        <Link href="/sign-in"> Sign In </Link>
-        <Link href="/explore"> Explore </Link>
-        <Link href="/profile"> Profile </Link>
-        <Link href="/properties/1"> Property </Link>
     </View>
   );
 }
